ee95e9996b088af6a9ee88d7d8235330268f5e12
Purnendu-sarkar <dsr102.purnendu@gmail.com>
2025-02-20 21:03:43 +0600

    Initial project setup and test completion.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div className="text-3xl font-bold text-center text-blue-500">
+        Tailwind CSS is Working!
+      </div>
+      <div className="flex justify-center items-center h-screen">
+        <button className="btn btn-primary">DaisyUI Button</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
